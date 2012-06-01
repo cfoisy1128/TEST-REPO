537a7e0cbdb49fddc7eb634a1b85b6328ee83e21
Christine Foisy <christinefoisy@new-host-2.home>
2012-06-01 14:46:47 -0400

    Added first files to the repo

---
+++ main.js
@@ -0,0 +1,7 @@
+// Hello World! - Deliverable 1
+// Author: Christine Foisy
+// Created for: SDI Online
+// Introductory look into Javascript using the Firebug console.
+// Our big adventure
+
+console.log("Hello World!");
